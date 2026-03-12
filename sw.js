c90eba8e75cbf83b90035d450085cfc652be7f56
fabricenkogheondzighe-commits <fabricenkogheondzighe@gmail.com>
2026-03-12 22:05:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monfermier-v202603122300';
+const CACHE = 'monfermier-v202603122330';
 const FILES = ['./', './index.html'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){}));
